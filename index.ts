ff28d61b2cd7d77e69cea7e7f6d9e0b8b86d9dde
Sujas <ce1221047@iitd.ac.in>
2024-12-14 18:18:12 +0530

    done

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import db from "./prisma/client";
 import {ProfileRouter} from "./src/routes/profile";
+import path from "path";
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
